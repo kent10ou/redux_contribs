a2272eb8abc1001ba7b31dfe69637b32f545e250
kent10ou <kent10ou@gmail.com>
2017-10-06 10:52:16 -0700

    using redux-promise-middleware, working on fetching github

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 export const FETCH_GITHUB = 'FETCH_GITHUB';
+export const FETCH_PENDING = 'FETCH_PENDING';
+export const FETCH_FULLFILLED = 'FETCH_FULLFILLED';
 
 export function fetchGithub () {
   return (dispatch) => {
-    
+    dispatch()
   }
 }
--- src/containers/contribs_grid.js
+++ src/containers/contribs_grid.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import fetchGithub from '../actions/index';
 
 class ContribsGrid extends Component {
   componentDidMount() {
-    
+    // fire action to fetch 
+    this.props.fetchGithub();
   }
 
   render() {
